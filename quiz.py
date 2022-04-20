19998b655be099fca1a7213c580e065afb115bc2
jony <jony04y01@gmail.com>
2022-04-20 17:33:59 +0300

    Replaced the background picture

--- quiz.py
+++ quiz.py
@@ -2,7 +2,7 @@ from tkinter import *
 import pygame
 import cv2
 from time import sleep
-from tkinter import messagebox as mb # For the result message box
+from tkinter import messagebox as mb  # For the result message box
 from PIL import ImageTk, Image
 import json
 
@@ -11,9 +11,9 @@ import json
 class Quiz:
 
     def __init__(self):
-         
+
         # set question number to 0
-        self.q_no=0
+        self.q_no = 0
 
         # assigns ques to the display_question function to update later.
         self.display_title()
@@ -21,55 +21,49 @@ class Quiz:
 
         # opt_selected holds an integer value which is used for
         # selected option in a question.
-        self.opt_selected=IntVar()
-         
+        self.opt_selected = IntVar()
+
         # displaying radio button for the current question and used to
         # display options for the current question
-        self.opts=self.radio_buttons()
-         
+        self.opts = self.radio_buttons()
+
         # display options for the current question
         self.display_options()
-         
+
         # displays the button for next and exit.
         self.buttons()
-         
+
         # no of questions
-        self.data_size=len(question)
-         
+        self.data_size = len(question)
+
         # keep a counter of correct answers
-        self.correct=0
-
-
+        self.correct = 0
 
     # This method is used to display the result
     # It counts the number of correct and wrong answers
     # and then display them at the end as a message Box
     def display_result(self):
-         
+
         # calculates the wrong count
         wrong_count = self.data_size - self.correct
         correct = f"Correct: {self.correct}"
         wrong = f"Wrong: {wrong_count}"
-         
+
         # calculates the percentage of correct answers
         score = int(self.correct / self.data_size * 100)
         result = f"Score: {score}%"
-         
+
         # Shows a message box to display the result
         if wrong_count < self.correct:
             pygame.mixer.music.load("nice.mp3")
             pygame.mixer.music.play(loops=0)
             mb.showinfo("Pretty good!", f"{result}\n{correct}\n{wrong}")
-            
-            
+
+
         else:
             pygame.mixer.music.load("trash.mp3")
             pygame.mixer.music.play(loops=0)
             mb.showinfo("Trash!", f"{result}\n{wrong}\n{correct}")
-           
-            
-    
-
 
     # This method checks the Answer after we click on Next.
     def check_ans(self, q_no):
@@ -130,7 +124,7 @@ class Quiz:
         # load the background music again
         pygame.mixer.music.load("background.mp3")
         pygame.mixer.music.play(loops=0)
- 
+
     # This method is used to check the answer of the
     # current question by calling the check_ans and question no.
     # if the question is correct it increases the count by 1
@@ -138,131 +132,122 @@ class Quiz:
     # question then it calls display result to show the message box.
     # otherwise shows next question.
     def next_btn(self):
-         
+
         # Check if the answer is correct
         if self.check_ans(self.q_no):
-             
             # if the answer is correct it increments the correct by 1
             self.correct += 1
-         
+
         # Moves to next Question by incrementing the q_no counter
         self.q_no += 1
-         
+
         # checks if the q_no size is equal to the data size
-        if self.q_no==self.data_size:
-             
+        if self.q_no == self.data_size:
+
             # if it is correct then it displays the score
             self.display_result()
-             
+
             # destroys the GUI
             gui.destroy()
         else:
             # shows the next question
             self.display_question()
             self.display_options()
- 
- 
-    
+
     def buttons(self):
-         
+
         # The first button is the Next button to move to the
         # next Question
-        next_button = Button(gui, text="Next",command=self.next_btn,
-        width=10,bg="blue",fg="white",font=("ariel",16,"bold"))
-         
+        next_button = Button(gui, text="Next", command=self.next_btn,
+                             width=10, bg="blue", fg="white", font=("ariel", 16, "bold"))
+
         # placing the button  on the screen
-        next_button.place(x=350,y=380)
-         
+        next_button.place(x=350, y=380)
+
         # This is the second button which is used to Quit the GUI
         quit_button = Button(gui, text="Quit", command=gui.destroy,
-        width=5,bg="black", fg="white",font=("ariel",16," bold"))
-         
+                             width=5, bg="black", fg="white", font=("ariel", 16, " bold"))
+
         # placing the Quit button on the screen
-        quit_button.place(x=700,y=50)
- 
- 
+        quit_button.place(x=700, y=50)
+
     # This method deselect the radio button on the screen
     # Then it is used to display the options available for the current
     # question which we obtain through the question number and Updates
     # each of the options for the current question of the radio button.
     def display_options(self):
-        val=0
-         
+        val = 0
+
         # deselecting the options
         self.opt_selected.set(0)
-         
+
         # looping over the options to be displayed for the
         # text of the radio buttons.
         for option in options[self.q_no]:
-            self.opts[val]['text']=option
-            val+=1
- 
- 
+            self.opts[val]['text'] = option
+            val += 1
+
     # This method shows the current Question on the screen
     def display_question(self):
-         
+
         # setting the Question properties
         q_no = Label(gui, text=question[self.q_no], width=60,
-        font=( 'ariel' ,16, 'bold' ), anchor= 'w' )
-         
-        #placing the option on the screen
+                     font=('ariel', 16, 'bold'), anchor='w')
+
+        # placing the option on the screen
         q_no.place(x=70, y=100)
- 
- 
+
     # This method is used to Display Title
     def display_title(self):
-         
+
         # The title to be shown
         title = Label(gui, text="Avi Kishta asks:",
-        width=50, bg="green",fg="white", font=("italic", 20, "bold"))
-         
+                      width=50, bg="green", fg="white", font=("italic", 20, "bold"))
+
         # place of the title
         title.place(x=0, y=2)
- 
- 
+
     # This method shows the radio buttons to select the Question
     # on the screen at the specified position. It also returns a
     # list of radio button which are later used to add the options to
     # them.
     def radio_buttons(self):
-         
+
         # initialize the list with an empty list of options
         q_list = []
-         
+
         # position of the first option
         y_pos = 150
-         
+
         # adding the options to the list
         while len(q_list) < 4:
-             
             # setting the radio button properties
-            radio_btn = Radiobutton(gui,text=" ",variable=self.opt_selected,
-            value = len(q_list)+1,font = ("ariel",14))
-             
+            radio_btn = Radiobutton(gui, text=" ", variable=self.opt_selected,
+                                    value=len(q_list) + 1, font=("ariel", 14))
+
             # adding the button to the list
             q_list.append(radio_btn)
-             
+
             # placing the button
-            radio_btn.place(x = 100, y = y_pos)
-             
+            radio_btn.place(x=100, y=y_pos)
+
             # incrementing the y-axis position by 40
             y_pos += 40
-         
+
         # return the radio buttons
         return q_list
-    
-   
-   
+
+
 # Create a GUI Window
 
 
 gui = Tk()
- 
+
 # set the size of the GUI Window
 gui.geometry("800x450")
 
-#Picture of Harel
-image1 = Image.open("picture_2.png")
+# Picture of Harel
+image1 = Image.open("pic.png")
 image1 = image1.resize((800, 450))
 test = ImageTk.PhotoImage(image1)
 
@@ -289,14 +274,14 @@ gui.title("Tannach Quiz")
 # get the data from the json file
 with open('data.json') as f:
     data = json.load(f)
- 
+
 # set the question, options, and answer
 question = (data['question'])
 options = (data['options'])
-answer = (data[ 'answer'])
- 
+answer = (data['answer'])
+
 # create an object of the Quiz Class.
 quiz = Quiz()
- 
+
 # Start the GUI
 gui.mainloop()
